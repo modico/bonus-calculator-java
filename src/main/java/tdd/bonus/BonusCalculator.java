66b5373ddca16ed3cebd59b4e39efa6285a810a7
Maciej Lewicki <maciej.lewicki@orange.com>
2021-06-22 11:33:25 +0200

    Implement individual bonus calculation

--- src/main/java/tdd/bonus/BonusCalculator.java
+++ src/main/java/tdd/bonus/BonusCalculator.java
@@ -1,5 +1,29 @@
 package tdd.bonus;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BonusCalculator {
 
+    public static final BigDecimal COMMISSION = BigDecimal.valueOf(0.1);
+    public static final BigDecimal TAX = BigDecimal.valueOf(0.1);
+
+    public BigDecimal calculate(BigDecimal sales, BigDecimal quota) {
+        if (sales.compareTo(quota) <= 0) {
+            return BigDecimal.valueOf(0);
+        }
+        return getCommissionNetto(getCommissionGross(sales, quota)).setScale(2, RoundingMode.DOWN);
+    }
+
+    private BigDecimal getCommissionNetto(BigDecimal commission) {
+        return commission.subtract(getTax(commission));
+    }
+
+    private BigDecimal getTax(BigDecimal commission) {
+        return commission.multiply(TAX);
+    }
+
+    private BigDecimal getCommissionGross(BigDecimal sales, BigDecimal quota) {
+        return sales.subtract(quota).multiply(COMMISSION);
+    }
 }
--- src/test/java/tdd/bonus/BonusCalculatorTest.java
+++ src/test/java/tdd/bonus/BonusCalculatorTest.java
@@ -2,12 +2,25 @@ package tdd.bonus;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BonusCalculatorTest {
 
     @Test
-    public void startHere() {
-        assertThat(4).isEqualTo(5);
+    public void no_commission_when_sales_equals_quota() {
+        assertThat(new BonusCalculator().calculate(BigDecimal.valueOf(15000),BigDecimal.valueOf(15000))).isEqualTo(BigDecimal.valueOf(0));
+    }
+
+    @Test
+    public void no_commission_when_sales_is_lower_then_quota() {
+        assertThat(new BonusCalculator().calculate(BigDecimal.valueOf(13000),BigDecimal.valueOf(15000))).isEqualTo(BigDecimal.valueOf(0));
+    }
+
+    @Test
+    public void ten_percent_commission_when_sales_is_higher_then_quota() {
+        assertThat(new BonusCalculator().calculate(BigDecimal.valueOf(12000),BigDecimal.valueOf(11000))).isEqualTo(BigDecimal.valueOf(90).setScale(2, RoundingMode.DOWN));
     }
 }
